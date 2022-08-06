624e898731d1c52b748da9a59a19fbd26d96bf61
Mateusz Szpyruk <mateuszszpyruk@gmail.com>
2022-08-07 01:05:27 +0200

    more results, one artist and link to a song

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -7,12 +8,21 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get('https://open.spotify.com/playlist/37i9dQZF1DWXRqgorJj26U')
 time.sleep(3)
+driver.execute_script("document.body.style.zoom = '0.04'")
+#scroll = driver.find_element(By.XPATH, "//div[@class='os-scrollbar-handle']")
+#print(scroll.value_of_css_property("transform"))
+#driver.execute_script("arguments[0].style.transform = 'translate(0px, 100.0000px)';", scroll)
+#print(scroll.value_of_css_property("transform"))
+time.sleep(3)
 search = driver.find_elements(By.XPATH, "//div[@class='JUa6JJNj7R_Y3i4P8YUX']")
-for piece in search:
-    content = piece.text
+for song in search:
+    content = song.text
 output = content.split("\n")
 output = list(filter(lambda x: x != 'E', output)) #removing "E" from list (explicit content)
 splits = np.array_split(output, len(output)/6)
 for array in splits:
+    link_song = driver.find_element(By.XPATH, "//div[@class='iCQtmPqY0QvkumAOuCjr']/a").get_attribute('href')
+    array = np.append(array, link_song)
+    array[2] = array[2].split(", ")[0]
     print(list(array))
 driver.close()
